51a612a9c9305cb87e7a3386e8093cc7f5d0677f
sahujeevika1101-maker <sahujeevika1101@gmail.com>
2026-02-23 13:37:28 +0530

    user class

--- main.py
+++ main.py
@@ -2,13 +2,23 @@
 
 # Book Class
 class Book:
-    def __init__(self, title, author, isbn):
+    def __init__(self, title, author,)
         self.title = title
         self.author = author
         self.isbn = isbn
         self.is_available = True
 
-    def __str__(self):
-        return f"{self.title} by {self.author} (ISBN: {self.isbn})"
+# user class
+class User:
+    def __init__(self, user_id, name):
+        self.user_id = user_id
+        self.name = name
+        self.borrowed_books = []
 
+    def borrow_book(self, book):
+        self.borrowed_books.append(book)
 
+    def return_book(self, book):
+        self.borrowed_books.remove(book)
+
+    
