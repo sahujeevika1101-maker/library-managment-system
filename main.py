85f603f471ce44fdc18849601afa4b516b099080
sahujeevika1101-maker <sahujeevika1101@gmail.com>
2026-02-23 13:24:08 +0530

    Book Class

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 # Library Management System
+
+# Book Class
+class Book:
+    def __init__(self, title, author, isbn):
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+        self.is_available = True
+
+    def __str__(self):
+        return f"{self.title} by {self.author} (ISBN: {self.isbn})"
+
+
